c44549254302997ba880eeb35aa49505b7330ef6
hayazaki <toru.hayazaki@gmail.com>
2024-07-14 07:38:44 +0900

    initial commit

---
+++ better_open_file_sample/android/app/src/main/kotlin/com/example/better_open_file_sample/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.better_open_file_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ open_file_sample/android/app/src/main/kotlin/com/example/open_file_sample/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.open_file_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
