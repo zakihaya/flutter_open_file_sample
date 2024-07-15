4ad92e0f8702690230efefa7c57255f8e6a93748
hayazaki <toru.hayazaki@gmail.com>
2024-07-15 09:09:43 +0900

    launch.json追加

--- better_open_file_sample/lib/main.dart
+++ better_open_file_sample/lib/main.dart
@@ -106,6 +106,9 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
+              'better_open_file_sample',
+            ),
+            const Text(
               'You have pushed the button this many times:',
             ),
             Text(
